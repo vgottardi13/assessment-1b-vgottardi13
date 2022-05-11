f24bd7359a83520ce15873b1b8a54557722bdfd1
vanessa_gottardi <vanessa.gottardi@live.vu.edu.au>
2022-05-11 23:28:54 +1000

    1. Just uploading assessments 2. created requested file 3. created groups with brackets. Didn't work. 4. used list() to create list from group

--- intersect.py
+++ intersect.py
@@ -1,6 +1,17 @@
 
-group_1 = ['80' '50' '63' '20' '75']
-group_2 = ['66' '50' '20' '1']
+group_1 = '80 50 63 20 75'
+group_2 = '66 50 20 1'
 
-print(group_1)
-print(group_2)
+list1 = list(group_1.split( ))
+list2 = list(group_2.split( ))
+
+print(list1)
+print(list2)
+
+print()
+
+for i in list1:
+    if i in(list2):
+        print(i, end=" ")
+
+
