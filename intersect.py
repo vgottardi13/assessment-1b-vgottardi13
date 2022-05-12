9e051109f1b8b448c3b8807a0f84bed9afe5ef55
vanessa_gottardi <vanessa.gottardi@live.vu.edu.au>
2022-05-12 16:54:40 +1000

    1. Just uploading assessments 2. created requested file 3. created groups with brackets. Didn't work. 4. used list() to create list from group 5. used split( ) to separate liat with space 6. used if() to find duplicates in both lists 7. used input() so users can pick their numbers

--- intersect.py
+++ intersect.py
@@ -4,14 +4,15 @@ group_2 = input('Enter 5 numbers separated by spaces: ')
 
 list1 = list(group_1.split( ))
 list2 = list(group_2.split( ))
-
+print()
 print(list1)
 print(list2)
-
 print()
-
+print("Duplicated Numbers:")
 for i in list1:
     if i in(list2):
         print(i, end=" ")
+    else print("No duplicated numbers")
+
 
 
